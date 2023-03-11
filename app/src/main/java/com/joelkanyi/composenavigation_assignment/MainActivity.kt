1507bca10666862d21e75c52863b2cb37dc2d9bc
Muasya <muasyapaul001@gmail.com>
2023-03-11 18:51:53 +0300

    Assignment

--- app/src/main/java/com/joelkanyi/composenavigation_assignment/MainActivity.kt
+++ app/src/main/java/com/joelkanyi/composenavigation_assignment/MainActivity.kt
@@ -3,6 +3,10 @@ package com.joelkanyi.composenavigation_assignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 import com.joelkanyi.composenavigation_assignment.ui.theme.ComposeNavigationAssignmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeNavigationAssignmentTheme {
-
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") { HomeScreen(navController = navController) }
+                    composable("details") { DetailsScreen(navController = navController) }
+                    composable("details") { RatingScreen(navController = navController) }
+                }
             }
         }
     }
